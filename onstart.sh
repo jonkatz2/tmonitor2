093bda3ff52fd2fc0bcc91f8380c4d49f50546a9
= <=>
2019-01-15 10:21:16 -0500

    manual commit

--- onstart.sh
+++ onstart.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
-sudo ntpdate-debian
+echo "######## TEMPERATURE MONITOR #########"
+echo "Checking server time..."
+sudo ntpdate-debian >> log.txt
 cd /home/pi/tmonitor2
+echo "Start monitoring"
+echo " " >> log.txt
+echo "-------------------------------------------------------" >> log.txt
 echo "$(date) Start" >> log.txt
 sudo python3 recordtemp.py
 
